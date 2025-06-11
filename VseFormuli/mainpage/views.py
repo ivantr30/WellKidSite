302ef6478a3cc3f7488c6432a85ac8f1ed104de8
ivanchikinator <102560801+ivanchikinator@users.noreply.github.com>
2025-06-11 20:08:51 +0300

    Delete VseFormuli directory

--- VseFormuli/mainpage/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def mainSite(request):
-    return render(request, template_name="main.html")
--- VseFormuli/subjectscontent/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class SubjectscontentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subjectscontent'
