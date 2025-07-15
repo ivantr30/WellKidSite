57d52193ebc25cda43f1a75e460ef82038ba9463
ivanchikinator <konfetnik2014@gmail.com>
2025-07-15 15:15:47 +0300

    Dz21beta1

--- VseFormuli/cinema/models.py
+++ VseFormuli/cinema/models.py
@@ -3,7 +3,7 @@ from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class Video(models.Model):
-    video = models.FileField("file", default=None, null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv', 'wmv'])])  # noqa: F821
+    video = models.FileField("file", validators=[FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv', 'wmv'])])  # noqa: F821
     title = models.CharField("title", max_length=50)
     preview = models.ImageField("preview", validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'PNG'])])
     category = models.CharField("category", max_length=50, default="Math")
