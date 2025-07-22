6a849cf24bd634896659d8fac603916b70f017ef
ivanchikinator <konfetnik2014@gmail.com>
2025-07-22 12:51:09 +0300

    Ending

--- VseFormuli/cinema/forms.py
+++ VseFormuli/cinema/forms.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Video
 
 class addVideoForm(ModelForm):
     class Meta:
         model = Video
         fields = "__all__"
+        widgets = {
+            'title' : TextInput(attrs={'placeholder' : 'Введите название'})
+        }
         
--- VseFormuli/cinema/models.py
+++ VseFormuli/cinema/models.py
@@ -5,8 +5,9 @@ from django.core.validators import FileExtensionValidator
 class Video(models.Model):
     video = models.FileField("file", upload_to='media/', validators=[FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv', 'wmv'])])  # noqa: F821
     title = models.CharField("title", max_length=50)
-    preview = models.ImageField("preview", upload_to='media/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'PNG'])])
-    category = models.CharField("category", max_length=50, default="Math")
+    preview = models.ImageField("preview", upload_to='media/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png', 'PNG', 'webp'])])
+    CATEGORIES = {'Math' :'Math', 'Geometry' : 'Geometry', 'Physics' : 'Physics', 'Algebra' : 'Algebra'}
+    category = models.CharField("category", max_length=50, default="Math", choices=CATEGORIES)
     class Meta():
         verbose_name = "Video"
         verbose_name_plural = "Videos"
